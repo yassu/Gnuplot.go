04db95b935162fd01a8337ee97a3991a9900f1b5
yassu <mathyassu@gmail.com>
2016-05-25 01:30:19 +0900

    Curve2d

--- gnuplot.go
+++ gnuplot.go
@@ -54,3 +54,33 @@ func (fun *Function2d) getData() [][2]float64 { // TODO: テスト書く
     }
     return a
 }
+
+var DefaultCurve2dSplitNum int = 100
+type Curve2d struct {
+    plotter Plotter
+    splitNum int
+    c func(float64) [2]float64
+}
+
+func (c *Curve2d) init(){
+    c.splitNum = DefaultCurve2dSplitNum
+    c.plotter.Configures = map[string] string {
+        "tMin": "-10.0",
+        "tMax": "10.0"}
+}
+
+func (c *Curve2d) getData() [][2]float64 { // TODO: test
+    tMin, _ := strconv.ParseFloat(c.plotter.Configures["tMin"], 32)
+    tMax, _ := strconv.ParseFloat(c.plotter.Configures["tMax"], 32)
+    var sep = float64(tMax - tMin) / float64(c.splitNum - 1)
+
+    var a [][2]float64
+    for j := 0; j < c.splitNum; j++ {
+        var t float64 = tMin + float64(j) * sep
+        cs := c.c(tMin + t * float64(j))
+        x0 := cs[0]
+        y0 := cs[1]
+        a = append(a, [2]float64{x0, y0})
+    }
+    return a
+}
