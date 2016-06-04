073214bc8a40e6fb65332da83544bf12862a57f0
yassu <mathyassu@gmail.com>
2016-06-05 03:45:42 +0900

    line color conf
    line color conf

--- conf/conf.go
+++ conf/conf.go
@@ -4,11 +4,36 @@ import (
 	"fmt"
 	"github.com/yassu/gnuplot.go/utils"
 	"regexp"
+	"strconv"
 )
 
 func isNum(s string) bool {
 	r := regexp.MustCompile(`^[+-]?[0-9]*[\.]?[0-9]+$`)
 	return r.MatchString(s)
+}
+
+func isIntStr(s string) bool {
+	r := regexp.MustCompile(`^[+-]?[0-9]+$`)
+	return r.MatchString(s)
+}
+
+func isSixHex(s string) bool {
+	r := regexp.MustCompile(`^[0-9a-f]{6}$`)
+	return r.MatchString(s)
+}
+
+func isEightHex(s string) bool {
+	r := regexp.MustCompile(`^[0-9a-f]{6}$`)
+	return r.MatchString(s)
+}
+
+// either s is float of 0 ~ 1 or not
+func isSmallFloat(s string) bool {
+	if !isNum(s) {
+		return false
+	}
+	f, _ := strconv.ParseFloat(s, 32)
+	return 0 <= f && f <= 1
 }
 
 // Configures
@@ -55,6 +80,73 @@ func WithConf() *Configure {
 			"lines", "dots", "steps", "errorbars", "xerrorbar",
 			"xyerrorlines", "points", "impulses", "fsteps", "errorlines", "xerrorlines",
 			"yerrorlines", "surface", "vectors", "parallelaxes"})
+	})
+}
+
+var COLOR_NAMES = []string{
+	"white", "black", "dark-grey", "red", "web-green", "web-blue",
+	"dark-magenta", "dark-cyan", "dark-orange", "dark-yellow", "royalblue",
+	"goldenrod", "dark-spring-green", "purple", "steelblue", "dark-red",
+	"dark-chartreuse", "orchild", "aquamarine", "brown", "yellow",
+	"turquoise",
+	"grey", "grey0", "grey10", "grey20", "grey30", "grey40", "grey50",
+	"grey60", "grey70", "grey80", "grey90", "grey100",
+	"light-red", "light-green", "light-blue", "light-magenta", "light-cyan",
+	"light-goldenrod", "light-pink", "light-turquoise", "gold", "green",
+	"dark-green", "sprint-green", "forest-green", "sea-green", "blue",
+	"dark-blue", "midnight-blue", "navy", "medium-blue", "skyblue",
+	"cyan", "magenta", "dark-turquoise", "dark-pink", "coral", "light-coral",
+	"orange-red", "salmon", "dark-salmon", "khaki", "dark-khaki",
+	"dark-goldenrod", "beige", "olive", "orange", "violet", "dark-violet",
+	"plum", "dark-plum", "dark-olivegreen", "orangered4", "brown4", "sienna4",
+	"orchid4", "mediumpurple3", "slateblue1", "yellow4", "sienna1", "tan1",
+	"standybrown", "light-salmon", "pink", "khaki1", "lemonchiffon", "bisque",
+	"honeydew", "slategrey", "seagreen", "antiquewhite", "chartreuse",
+	"greenyellow", "gray", "light-gray", "light-grey",
+	"dark-gray", "slategray",
+	"gray0", "gray10", "gray20", "gray30", "gray40", "gray50", "gray60",
+	"gray70", "gray80", "gray90", "gray100"}
+
+func LineColorConf() *Configure {
+	return NewConfigure([]string{"linecolor", "lc"}, []string{"1"}, func(vals []string) bool {
+		if len(vals) == 0 {
+			return false
+		}
+		// in case of linecolor "colorname"
+		val := vals[0]
+		if utils.InStr(val, COLOR_NAMES) {
+			return true
+		}
+
+		// in case of linecolor <n>
+		if isIntStr(val) {
+			return true
+		}
+
+		// in case of linecolor <colorspec> and len(vals) == 1
+		if utils.InStr(val, []string{"variable", "bgnd", "black"}) {
+			return true
+		}
+
+		if len(vals) == 2 && vals[0] == "rgbcolor" && utils.InStr(vals[1], COLOR_NAMES) {
+			return true
+		}
+		if len(vals) == 2 && vals[0] == "rgbcolor" && vals[1][0:2] == "0x" && isSixHex(vals[1][2:]) {
+			return true
+		}
+		if len(vals) == 2 && vals[0] == "rgbcolor" && vals[1][0:2] == "0x" && isEightHex(vals[1][2:]) {
+			return true
+		}
+		if len(vals) == 2 && vals[0] == "rgbcolor" && vals[1][0] == '#' && isSixHex(vals[1][1:]) {
+			return true
+		}
+		if len(vals) == 2 && vals[0] == "rgbcolor" && vals[1][0] == '#' && isEightHex(vals[1][1:]) {
+			return true
+		}
+		if len(vals) == 2 && vals[0] == "rgbcolor" && isIntStr(vals[1]) {
+			return true
+		}
+		return false
 	})
 }
 
